50c111bf1489a00c841733509410e435504b331c
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2024-02-28 22:09:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+    const filePath = path.resolve(__dirname, 'index.html');
+    res.sendFile(filePath);
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
